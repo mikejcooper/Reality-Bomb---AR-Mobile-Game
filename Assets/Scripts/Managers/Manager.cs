aac9333b3a217e5e58f93bb7bc463d5c8b859908
gchurch <gc14768@my.bristol.ac.uk>
2017-01-25 17:12:51 +0000

    simple cube trigger

---
+++ Assets/Scripts/Managers/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Manager : MonoBehaviour
+{
+	public GameObject m_Tank;
+	public GameObject m_Cube1;
+
+	// This is called from start and will run each phase of the game one after another.
+	private IEnumerator GameLoop ()
+	{
+		// Start off by running the 'RoundStarting' coroutine but don't return until it's finished.
+		yield return StartCoroutine (RoundStarting ());
+
+	}
+}
+
---
+++ Assets/collision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour {
+
+
+	private bool triggered = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Destroy (other.gameObject);
+		triggered = true;
+	}
+
+	public bool IsTriggered() {
+		return triggered;
+	}
+
+}
