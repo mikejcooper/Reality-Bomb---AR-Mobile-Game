38082f6a8d7a7031983229223758f1c3e4b6bf59
gchurch <gc14768@my.bristol.ac.uk>
2017-01-25 17:58:51 +0000

    detects if you entered active cube

--- Assets/Scripts/Managers/Manager.cs
+++ Assets/Scripts/Managers/Manager.cs
@@ -6,12 +6,21 @@ using UnityEngine.UI;
 public class Manager : MonoBehaviour
 {
 	public GameObject m_Tank;
-	public Collision m_Cube1;
+	public Collision[] m_Cubes;
+	public Collision m_ActiveCube;
+	public int activeCube;
+
+	void Start() {
+		//m_ActiveCube = m_Cubes [0];
+		activeCube = 0;
+	}
 
 	// This is called from start and will run each phase of the game one after another.
 	void Update() {
-		if (m_Cube1.IsTriggered() == true) {
-			Destroy (m_Tank);
+		if (m_Cubes[activeCube].IsTriggered () == true) {
+			print ("cube " + activeCube + " entered\n");
+			activeCube = (activeCube + 1) % m_Cubes.Length;
+			m_Cubes [activeCube].TriggerReset ();
 		}
 	}
 }
