54b4767eca5a6f130685665e5117eea8b74b1812
gchurch <gc14768@my.bristol.ac.uk>
2017-01-26 13:52:31 +0000

    using OnTriggerExit, cleaned up code

--- Assets/Scripts/Managers/Manager.cs
+++ Assets/Scripts/Managers/Manager.cs
@@ -7,20 +7,17 @@ public class Manager : MonoBehaviour
 {
 	public GameObject m_Tank;
 	public Collision[] m_Cubes;
-	public Collision m_ActiveCube;
-	public int activeCube;
+	public int m_ActiveCubeIndex;
 
-	void Start() {
-		//m_ActiveCube = m_Cubes [0];
-		activeCube = 0;
+	void Awake() {
+		m_ActiveCubeIndex = 0;
 	}
 
 	// This is called from start and will run each phase of the game one after another.
 	void Update() {
-		if (m_Cubes[activeCube].IsTriggered () == true) {
-			print ("cube " + activeCube + " entered\n");
-			activeCube = (activeCube + 1) % m_Cubes.Length;
-			m_Cubes [activeCube].TriggerReset ();
+		if (m_Cubes[m_ActiveCubeIndex].IsTriggered () == true) {
+			print ("cube " + m_ActiveCubeIndex + " entered\n");
+			m_ActiveCubeIndex = (m_ActiveCubeIndex + 1) % m_Cubes.Length;
 		}
 	}
 }
