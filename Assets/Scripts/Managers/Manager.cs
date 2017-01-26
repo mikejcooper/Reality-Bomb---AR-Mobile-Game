76498882d8195d74f36f0a1cbc1aa073e113331a
gchurch <gc14768@my.bristol.ac.uk>
2017-01-26 14:08:07 +0000

    only active cube is visible

--- Assets/Scripts/Managers/Manager.cs
+++ Assets/Scripts/Managers/Manager.cs
@@ -13,11 +13,17 @@ public class Manager : MonoBehaviour
 		m_ActiveCubeIndex = 0;
 	}
 
+	void Start() {
+		m_Cubes [m_ActiveCubeIndex].rend.enabled = true;
+	}
+
 	// This is called from start and will run each phase of the game one after another.
 	void Update() {
 		if (m_Cubes[m_ActiveCubeIndex].IsTriggered () == true) {
 			print ("cube " + m_ActiveCubeIndex + " entered\n");
+			m_Cubes [m_ActiveCubeIndex].rend.enabled = false;
 			m_ActiveCubeIndex = (m_ActiveCubeIndex + 1) % m_Cubes.Length;
+			m_Cubes [m_ActiveCubeIndex].rend.enabled = true;
 		}
 	}
 }
