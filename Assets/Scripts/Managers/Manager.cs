0354e53843b18df5137db30a9179e0aba952bd65
gchurch <gc14768@my.bristol.ac.uk>
2017-01-26 14:28:08 +0000

    5 seconds to enter active cube

--- Assets/Scripts/Managers/Manager.cs
+++ Assets/Scripts/Managers/Manager.cs
@@ -8,22 +8,36 @@ public class Manager : MonoBehaviour
 	public GameObject m_Tank;
 	public Collision[] m_Cubes;
 	public int m_ActiveCubeIndex;
+	private float m_TimeLeft;
 
 	void Awake() {
 		m_ActiveCubeIndex = 0;
+		m_TimeLeft = 5.0f;
 	}
 
 	void Start() {
 		m_Cubes [m_ActiveCubeIndex].rend.enabled = true;
 	}
 
+
 	// This is called from start and will run each phase of the game one after another.
 	void Update() {
-		if (m_Cubes[m_ActiveCubeIndex].IsTriggered () == true) {
-			print ("cube " + m_ActiveCubeIndex + " entered\n");
-			m_Cubes [m_ActiveCubeIndex].rend.enabled = false;
-			m_ActiveCubeIndex = (m_ActiveCubeIndex + 1) % m_Cubes.Length;
-			m_Cubes [m_ActiveCubeIndex].rend.enabled = true;
+		m_TimeLeft -= Time.deltaTime;
+
+		if (m_TimeLeft < 0) {
+			print ("Game Over!");
+			Destroy (m_Tank);
+		} else {
+			print (m_TimeLeft);
+			//if active cube is entered
+			if (m_Cubes [m_ActiveCubeIndex].IsTriggered () == true) {
+				print ("cube " + m_ActiveCubeIndex + " entered\n");
+				m_Cubes [m_ActiveCubeIndex].rend.enabled = false;
+				//update active cube
+				m_ActiveCubeIndex = (m_ActiveCubeIndex + 1) % m_Cubes.Length;
+				m_Cubes [m_ActiveCubeIndex].rend.enabled = true;
+				m_TimeLeft += 5.0f;
+			}
 		}
 	}
 }
