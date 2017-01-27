c7bbe741b13f6bda6b64754739e848d8ee9e2821
gchurch <gc14768@my.bristol.ac.uk>
2017-01-27 13:56:29 +0000

    display score and reset on game over

--- Assets/Scripts/Managers/Manager.cs
+++ Assets/Scripts/Managers/Manager.cs
@@ -10,12 +10,17 @@ public class Manager : MonoBehaviour
 	public int m_ActiveCubeIndex;	//Index of the Currently Active Trigger
 	private float m_TimeLeft;		//Remaining time to get to Trigger Zone
 	private Text m_TimeLeftText;
+	private int m_Score;
+	private Text m_ScoreText;
 
 	void Awake() {
 		m_ActiveCubeIndex = 0;		//Initialises Cube0 as first trigger zone
 		m_TimeLeft = 5.0f;			//Initialises time on clock to 5 secs
 		m_TimeLeftText = GameObject.Find("TimeLeftText").gameObject.GetComponent<Text>();
-		m_TimeLeftText.text = string.Format("{0:N2}", m_TimeLeft);
+		m_TimeLeftText.text = "Time Left: " + string.Format("{0:N2}", m_TimeLeft);
+		m_Score = 0;
+		m_ScoreText = GameObject.Find("ScoreText").gameObject.GetComponent<Text>();
+		m_ScoreText.text = "Score: " + m_Score;
 	}
 
 	void Start() {
@@ -26,11 +31,12 @@ public class Manager : MonoBehaviour
 	// This is called from start and will run each phase of the game one after another.
 	void Update() {
 		m_TimeLeft -= Time.deltaTime;	//Subtracts the elapsed time from the remaining time
-		m_TimeLeftText.text = string.Format("{0:N2}", m_TimeLeft);
+		m_TimeLeftText.text = "Time Left: " + string.Format("{0:N2}", m_TimeLeft);
+		m_ScoreText.text = "Score: " + m_Score;
 
 		if (m_TimeLeft < 0) {			//If time has expired call game over condition
-			print ("Game Over!");
-			Destroy (m_Tank);
+			m_TimeLeft = 5.0f;
+			m_Score = 0;
 		} else {						//If time has not yet expired
 			//print (m_TimeLeft);
 			//if active cube is entered
@@ -41,6 +47,7 @@ public class Manager : MonoBehaviour
 				m_ActiveCubeIndex = (m_ActiveCubeIndex + 1) % m_Cubes.Length;	//Active the next trigger zone
 				m_Cubes [m_ActiveCubeIndex].rend.enabled = true;		//Enable visibility of newly active trigger
 				m_TimeLeft += 5.0f;		//Increment time left by 5 secs
+				m_Score++;
 			}
 		}
 	}
