c0f2d98ea537cb8fa42b7e71e4c88000d15d15f9
Jose Vanterpool <Jose@eduroam-57-136.nomadic.bris.ac.uk>
2017-01-27 15:30:51 +0000

    added falling object script

---
+++ Assets/FallingObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class FallingObject : MonoBehaviour {
+
+	public Renderer rend;			//Used to enable and disable visibility of trigger
+	public float fallSpeed = 8.0f;
+	public float spinSpeed = 250.0f;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		rend.enabled = true;
+	}
+
+	void Update() {
+
+		transform.Translate(Vector3.down * fallSpeed * Time.deltaTime, Space.World);
+		transform.Rotate(Vector3.forward, spinSpeed * Time.deltaTime);
+
+	}
+
+	void OnTriggerEnter(Collider other) {	//When player enters trigger zone
+		rend.enabled = false;
+		print ("sphere hit tank");
+	}
+
+}
+
