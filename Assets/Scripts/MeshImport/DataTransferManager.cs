847d5a3f0517ca938ac305d9f876e5e506247a5a
Rory Smith <roarsmith3@gmail.com>
2017-01-30 20:40:47 +0000

    Added documentation for object importer.

--- Assets/Scripts/MeshImport/DataTransferManager.cs
+++ Assets/Scripts/MeshImport/DataTransferManager.cs
@@ -62,30 +62,35 @@ public class DataTransferManager : MonoBehaviour {
 	void handleMesh (string data) {
 		UnityThreadHelper.Dispatcher.Dispatch(() =>
 			{
-
+				// choose the material - we can get round to using a custom invisible
+				// shader at some point here, but for development purposes it's nice
+				// to be able to see the mesh
 				Material material = Resources.Load("Materials/MeshDefault", typeof(Material)) as Material;
 
+				// convert the mesh object string into an actual Unity mesh
 				mesh = FastObjImporter.Instance.ImportString(data);
-
 				mesh.RecalculateBounds();
-				;
-				Debug.Log("creating gameobject");
-				GameObject moduleGo = new GameObject(); //create the game object
-				MeshFilter filter = moduleGo.AddComponent<MeshFilter>(); //add the meshfilter
-				filter.mesh = mesh; //assign the mesh
-				moduleGo.AddComponent<MeshRenderer>(); //and mesh renderer
 
+				GameObject worldMesh = new GameObject("world mesh");
+				MeshFilter filter = worldMesh.AddComponent<MeshFilter>();
+				filter.mesh = mesh;
+				worldMesh.AddComponent<MeshRenderer>();
+
+				// attach to Marker scene
 				GameObject root = GameObject.Find("Marker scene");
-				Debug.Log (material);
-				Debug.Log("adding gameobject");
-				moduleGo.transform.parent = root.transform;
+				worldMesh.transform.parent = root.transform;
 
-
-				MeshRenderer meshRenderer = moduleGo.GetComponent<MeshRenderer>();
+				// set mesh material
+				MeshRenderer meshRenderer = worldMesh.GetComponent<MeshRenderer>();
 				meshRenderer.material = material;
 
+				// set visibility based on whether or not the marker is currently visible
+				GameObject toolkit = GameObject.Find("ARToolKit");
+				ARMarker marker = toolkit.GetComponent<ARMarker>();
+				worldMesh.SetActive(marker.Visible);
 
-				SetLayerRecursively(moduleGo, 9);
+				// assign to correct layer for ArToolKit
+				SetLayerRecursively(worldMesh, 9);
 
 
 			});
@@ -95,24 +100,33 @@ public class DataTransferManager : MonoBehaviour {
 		
 		UnityThreadHelper.Dispatcher.Dispatch(() =>
 			{
-				string filename = System.IO.Path.Combine(Application.persistentDataPath, "markers.dat");
+				// find the path we'll store the marker file under
+				string filename = "markers.dat";
+				string filepath = System.IO.Path.Combine(Application.persistentDataPath, filename);
 
-				using(TextWriter writer = new StreamWriter(filename))
+				// save to the file, we can't pass the file by string to ARToolKit
+				// because ARToolKit calls a native method that processes the file
+				// based off a file path. So we must save to a file, to pass a path
+				using(TextWriter writer = new StreamWriter(filepath))
 				{
 					writer.Write(data);
 					writer.Close();
 				}
 
+				// find the ARToolKit object and associated marker component
 				GameObject toolkit = GameObject.Find("ARToolKit");
-
 				ARMarker marker = toolkit.GetComponent<ARMarker>();
 
+				// temporarily disable any existing instance
 				marker.enabled = false;
 
+				// use our custom field to avoid the file being loaded from
+				// StreamingAssets by ARToolKit
 				marker.MarkerType = MarkerType.Multimarker;
-				marker.MultiConfigFile = "markers.dat";
-				marker.MultiConfigNonStreamingFile = filename;
+				marker.MultiConfigFile = filename;
+				marker.MultiConfigNonStreamingFile = filepath;
 
+				// enable the marker so that ARToolKit knows params have changed
 				marker.enabled = true;
 			});
 	}
---
+++ Assets/Scripts/MeshImport/readme.md
@@ -0,0 +1,3 @@
+# MeshImport
+
+To load meshes during the game, go to the `dev_server~` directory. This is hidden in the Unity GUI, so you'll need to find it in your file manager / terminal. From there, run npm install to install the required Node JS modules and then `node app.js` to run the mesh server. Now when you run the game you should see a mesh get imported when it runs.
