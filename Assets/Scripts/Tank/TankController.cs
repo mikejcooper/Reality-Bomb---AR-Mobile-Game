0ae0e0eb074e17e7fa4dbc4dfed99ffb3819446b
Jose Vanterpool <Jose@eduroam-82-252.nomadic.bris.ac.uk>
2017-02-03 15:12:24 +0000

    Added tankmovement and tank controller

---
+++ Assets/Scripts/Tank/TankController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class TankController : NetworkBehaviour
+{
+	public bool isPlayingSolo = false; // temporary hack to allow tank prefab to be spawned and played without a network system
+
+    public int m_PlayerNumber = 1;              // Used to identify which tank belongs to which player.  This is set by this tank's manager.
+    public float m_Speed = 3f;                 // How fast the tank moves forward and back.
+    public float m_TurnSpeed = 180f;            // How fast the tank turns in degrees per second.
+   
+    private UIJoystick m_Joystick;
+    private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
+    private string m_TurnAxisName;              // The name of the input axis for turning.
+    private Rigidbody m_Rigidbody;              // Reference used to move the tank.
+    private Vector3 m_Direction;
+
+	private Quaternion lookAngle;
+
+
+    private void Awake ()
+    {
+        m_Rigidbody = GetComponent<Rigidbody> ();
+		m_Joystick = GameObject.Find("JoystickBack").gameObject.GetComponent<UIJoystick>();
+    }
+
+
+    private void OnEnable ()
+    {
+        // When the tank is turned on, make sure it's not kinematic.
+        m_Rigidbody.isKinematic = false;
+
+        // Also reset the input values.
+    }
+
+
+    private void OnDisable ()
+    {
+        // When the tank is turned off, set it to kinematic so it stops moving.
+        m_Rigidbody.isKinematic = true;
+    }
+
+
+    private void Start ()
+    {
+        m_Joystick = GameObject.Find("JoystickBack").gameObject.GetComponent<UIJoystick>();
+        // The axes names are based on player number.
+        m_MovementAxisName = "Vertical" + m_PlayerNumber;
+		m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+    }
+
+
+    private void Update ()
+    {
+		// we should find a proper way to spawn tanks so we don't need to rely
+		// on isPlayingSolo
+        if (!isLocalPlayer && !isPlayingSolo)
+        { 
+            return;
+        }
+
+    }
+
+    private void FixedUpdate ()
+    {
+
+		Vector3 joystickVector = new Vector3 (m_Joystick.Horizontal (), m_Joystick.Vertical (), 0);
+		GameObject ARCamera = GameObject.Find ("ARCamera");
+		Vector3 rotatedVector = ARCamera.transform.rotation * joystickVector;
+
+
+		if (m_Joystick.IsDragging ()) {
+			lookAngle = Quaternion.FromToRotation (Vector3.forward, rotatedVector);
+			// think about combining z and y so that it moves away when close to 0 degrees
+			float combined = lookAngle.eulerAngles.y;
+			lookAngle.eulerAngles = new Vector3(0, combined, 0);
+		}
+
+		m_Rigidbody.rotation = lookAngle;
+
+		Vector3 movement = transform.forward * joystickVector.magnitude * m_Speed * Time.deltaTime;
+
+		m_Rigidbody.position += movement;
+
+    }
+
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Tank") {
+			print ("Hit Tank!");
+		}
+	}
+
+}
