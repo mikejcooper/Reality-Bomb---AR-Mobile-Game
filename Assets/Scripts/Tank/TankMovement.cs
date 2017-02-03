ba7aba172cba6984e92088dbd1ca7a242f458934
Rory Smith <roarsmith3@gmail.com>
2017-02-03 13:48:57 +0000

    Changed TankMovement to incorporate camera rotation.

--- Assets/Scripts/Tank/TankMovement.cs
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -18,8 +18,8 @@ public class TankMovement : NetworkBehaviour
     private string m_MovementAxisName;          // The name of the input axis for moving forward and back.
     private string m_TurnAxisName;              // The name of the input axis for turning.
     private Rigidbody m_Rigidbody;              // Reference used to move the tank.
-    private float m_MovementInputValue;         // The current value of the movement input.
-    private float m_TurnInputValue;             // The current value of the turn input.
+//    private float m_MovementInputValue;         // The current value of the movement input.
+//    private float m_TurnInputValue;             // The current value of the turn input.
     private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
 
 
@@ -36,8 +36,8 @@ public class TankMovement : NetworkBehaviour
         m_Rigidbody.isKinematic = false;
 
         // Also reset the input values.
-        m_MovementInputValue = 0f;
-        m_TurnInputValue = 0f;
+//        m_MovementInputValue = 0f;
+//        m_TurnInputValue = 0f;
     }
 
 
@@ -69,74 +69,100 @@ public class TankMovement : NetworkBehaviour
             return;
         }
         // Store value of both input axes from joystick
-        m_MovementInputValue  = m_Joystick.Vertical();
-		m_TurnInputValue = m_Joystick.Horizontal ();
 
-        EngineAudio ();
+
+//        EngineAudio ();
     }
 
-
-    private void EngineAudio ()
-    {
-        // If there is no input (the tank is stationary)...
-        if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
-        {
-            // ... and if the audio source is currently playing the driving clip...
-            if (m_MovementAudio.clip == m_EngineDriving)
-            {
-                // ... change the clip to idling and play it.
-                m_MovementAudio.clip = m_EngineIdling;
-                m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                m_MovementAudio.Play ();
-            }
-        }
-        else
-        {
-            // Otherwise if the tank is moving and if the idling clip is currently playing...
-            if (m_MovementAudio.clip == m_EngineIdling)
-            {
-                // ... change the clip to driving and play.
-                m_MovementAudio.clip = m_EngineDriving;
-                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
-                m_MovementAudio.Play();
-            }
-        }
-    }
+//
+//    private void EngineAudio ()
+//    {
+//        // If there is no input (the tank is stationary)...
+//        if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
+//        {
+//            // ... and if the audio source is currently playing the driving clip...
+//            if (m_MovementAudio.clip == m_EngineDriving)
+//            {
+//                // ... change the clip to idling and play it.
+//                m_MovementAudio.clip = m_EngineIdling;
+//                m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+//                m_MovementAudio.Play ();
+//            }
+//        }
+//        else
+//        {
+//            // Otherwise if the tank is moving and if the idling clip is currently playing...
+//            if (m_MovementAudio.clip == m_EngineIdling)
+//            {
+//                // ... change the clip to driving and play.
+//                m_MovementAudio.clip = m_EngineDriving;
+//                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+//                m_MovementAudio.Play();
+//            }
+//        }
+//    }
 
 
     private void FixedUpdate ()
     {
-        // Adjust the rigidbodies position and orientation in FixedUpdate.
-		Turn ();
-	
-		Move ();
-    }
+//		m_MovementInputValue  = m_Joystick.Vertical();
+//		m_TurnInputValue = m_Joystick.Horizontal ();
+
+		// face the direction of the joystick
+
+		var angle = 90 - Mathf.Atan2(m_Joystick.Vertical(), m_Joystick.Horizontal()) * Mathf.Rad2Deg;
+//		Debug.Log(string.Format ("({0} , {1}) => {2}", m_Joystick.Horizontal(), m_Joystick.Vertical(), angle));
+
+		Vector3 joystickVector = new Vector3 (m_Joystick.Horizontal (), m_Joystick.Vertical (), 0);
 
 
-    private void Move ()
-    {
-		// Get direction from the absolute joysitck input values. 
-		float direction = Mathf.Min(1,Mathf.Abs(m_MovementInputValue) + Mathf.Abs(m_TurnInputValue));
+		GameObject ARCamera = GameObject.Find ("ARCamera");
+
+//		Debug.Log (ARCamera.transform.rotation.eulerAngles);
+
+		Vector3 rotatedVector = ARCamera.transform.rotation * joystickVector;
+
+		// think about combining z and y so that it moves away when close to 0 degrees
+
+		Quaternion lookAngle = Quaternion.FromToRotation (Vector3.forward, rotatedVector);
+//		Debug.Log (string.Format ("{0} => {1} => {2}", joystickVector, rotatedVector, lookAngle.eulerAngles));
+
+//		m_Rigidbody.rotation = ARCamera.transform.rotation * Quaternion.AngleAxis (angle, new Vector3 (0, 1, 0)); // probably should use tank normal rather than y axis
+
+//		m_Rigidbody.rotation = Quaternion.FromToRotation (new Vector3 (1, 1, 1), new Vector3 (1, 1, 1));
+		lookAngle.eulerAngles = new Vector3(0, lookAngle.eulerAngles.y, 0);
+		m_Rigidbody.rotation = lookAngle;
+
+
+
+
+		Vector3 movement = transform.forward * m_Speed * Time.deltaTime;
+
+		Debug.Log (string.Format ("{0} => {1} => {2}", lookAngle.eulerAngles, movement, movement*joystickVector.magnitude));
+		movement *= joystickVector.magnitude;
+
+		m_Rigidbody.position += movement;
+
+//		float Joystick_angle = Mathf.Atan2(m_TurnInputValue, m_MovementInputValue) * Mathf.Rad2Deg; 
+//		float Camera_angle = 0;//Mathf.Atan2(Camera.main.transform.rotation.x, Camera.main.transform.rotation.y) * Mathf.Rad2Deg; 
+//		// Maths not quite right here?? "Seems to work" when 2*Camera_angle
+//		float Direction_angle = Joystick_angle + 2*Camera_angle;
+//		if (Joystick_angle == 0) {
+//			float Tank_angle = m_Rigidbody.transform.rotation.eulerAngles.y;
+//			Direction_angle = Tank_angle;
+//		}
+//
+//		// Apply this rotation to the rigidbody's rotation.
+//		m_Rigidbody.rotation = Quaternion.Euler(new Vector3(0f, Direction_angle, 0f));
+//		m_Rigidbody.MoveRotation (m_Rigidbody.rotation);
+//	
+//		// Get direction from the absolute joysitck input values. 
+//		float direction = Mathf.Min(1,Mathf.Abs(m_MovementInputValue) + Mathf.Abs(m_TurnInputValue));
 		// Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames.
-		Vector3 movement = transform.forward * direction * m_Speed * Time.deltaTime;
+//		Vector3 movement = transform.forward * direction * m_Speed * Time.deltaTime;
 		// Apply this movement to the rigidbody's position.
-		m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+//		m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
     }
 
-    private void Turn ()
-    {
 
-		float Joystick_angle = Mathf.Atan2(m_TurnInputValue, m_MovementInputValue) * Mathf.Rad2Deg; 
-		float Camera_angle = 0;//Mathf.Atan2(Camera.main.transform.rotation.x, Camera.main.transform.rotation.y) * Mathf.Rad2Deg; 
-		// Maths not quite right here?? "Seems to work" when 2*Camera_angle
-		float Direction_angle = Joystick_angle + 2*Camera_angle;
-		if (Joystick_angle == 0) {
-			float Tank_angle = m_Rigidbody.transform.rotation.eulerAngles.y;
-			Direction_angle = Tank_angle;
-		}
-
-        // Apply this rotation to the rigidbody's rotation.
-		m_Rigidbody.rotation = Quaternion.Euler(new Vector3(0f, Direction_angle, 0f));
-		m_Rigidbody.MoveRotation (m_Rigidbody.rotation);
-    }
 }
