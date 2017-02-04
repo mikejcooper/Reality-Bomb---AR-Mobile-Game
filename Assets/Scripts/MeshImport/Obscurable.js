240b65d83f541b241041814b9c857c24e33c58ea
Rory Smith <roarsmith3@gmail.com>
2017-02-04 16:21:03 +0000

    Added an obscurable shader which is invisible but can obscure objects.

---
+++ Assets/Scripts/MeshImport/Obscurable.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+function Start () {
+	var renders = GetComponentsInChildren(Renderer);
+	for (var rendr : Renderer in renders){
+		rendr.material.renderQueue = 2002; // set their renderQueue
+	}
+}
