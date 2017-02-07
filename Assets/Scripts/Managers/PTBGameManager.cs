75fc956bfafd5da60186584e6c9d6846d1df39cc
Jose Vanterpool <Jose@eduroam-82-252.nomadic.bris.ac.uk>
2017-02-07 15:20:44 +0000

    updated mesh scene

--- Assets/Scripts/Managers/PTBGameManager.cs
+++ Assets/Scripts/Managers/PTBGameManager.cs
@@ -4,20 +4,50 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class PTBGameManager : NetworkManager {
+
+	private bool s_root;
     private int players = 0;
+	private GameObject tankObject;
+
+	private void Awake(){
+		s_root = GameObject.Find("Scene Root").gameObject.GetComponent<DataTransferManager>();
+	}
+
+	private void Update(){
+		
+	}
+
+//	[ClientRpc]
+//	public void spawnPlayer(GameObject mesh) {
+//		tankObject.transform = mesh.transform;
+//		tankObject.transform.position.y += mesh.GetComponent<MeshRenderer> ().bounds.size.y/2;
+//		// use the tank references to spawn to some location
+//
+//		// use center of mesh with random
+//
+//		// show
+//		tankObject.SetActive (true);
+//	}
+
 	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
 	{
-		var pos = GetStartPosition ();
-		//Debug.Log (pos);
-		var player = (GameObject)GameObject.Instantiate (playerPrefab, pos.position, Quaternion.identity);
+//		var pos = GetStartPosition ();
+//		Debug.Log (pos);
+
+		tankObject = (GameObject)GameObject.Instantiate (playerPrefab, new Vector3(0,0,0), Quaternion.identity);
 		
 
         players++;
         Debug.Log("Players = " + players);
         if (players == 2)
         {
-            player.gameObject.GetComponent<TankController>().hasBomb = true;
+			tankObject.gameObject.GetComponent<TankController>().hasBomb = true;
         }
-        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+		NetworkServer.AddPlayerForConnection(conn, tankObject, playerControllerId);
+
+		// hides the tank when its spawned
+		tankObject.SetActive (false);
+
+
     }
 }
