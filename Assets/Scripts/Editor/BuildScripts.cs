a768605c292fb49bc7ac89aba0b71d1c059715c4
Matthew Duffin <matthewjduffin@gmail.com>
2017-02-14 14:58:09 +0000

    Added Windows builds to script

--- Assets/Scripts/Editor/BuildScripts.cs
+++ Assets/Scripts/Editor/BuildScripts.cs
@@ -39,4 +39,40 @@ public class BuildScripts
 		proc.StartInfo.FileName = path + "client.app";
 		proc.Start();
 	}
+
+    [MenuItem("Builds/Windows TV")]
+    public static void BuildWindowsTV()
+    {
+        // Get filename.
+        string path = "";
+
+        string[] levels = new string[] { "Assets/_Scenes/tv/Idle.unity", "Assets/_Scenes/shared/Game.unity", "Assets/_Scenes/tv/Leaderboard.unity" };
+
+        // Build player.
+        BuildPipeline.BuildPlayer(levels, path + "TV.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+
+
+        // Run the game (Process class from System.Diagnostics).
+        Process proc = new Process();
+        proc.StartInfo.FileName = path + "TV.exe";
+        proc.Start();
+    }
+
+    [MenuItem("Builds/Windows client")]
+    public static void BuildWindowsClient()
+    {
+        // Get filename.
+        string path = "";
+
+        string[] levels = new string[] { "Assets/_Scenes/mobile/Idle.unity", "Assets/_Scenes/mobile/MiniGame.unity", "Assets/_Scenes/shared/Game.unity", "Assets/_Scenes/mobile/Leaderboard.unity" };
+
+        // Build player.
+        BuildPipeline.BuildPlayer(levels, path + "client.exe", BuildTarget.StandaloneWindows64, BuildOptions.None);
+
+
+        // Run the game (Process class from System.Diagnostics).
+        Process proc = new Process();
+        proc.StartInfo.FileName = path + "client.exe";
+        proc.Start();
+    }
 }
