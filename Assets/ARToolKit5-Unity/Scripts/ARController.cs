c1415d85c040b4fdb6fb83955e603f5189afad62
Matthew Duffin <matthewjduffin@gmail.com>
2017-02-24 20:41:50 +0000

    Moved InitializeAR back to Start(). Not sure what effect this will have on iOS

--- Assets/ARToolKit5-Unity/Scripts/ARController.cs
+++ Assets/ARToolKit5-Unity/Scripts/ARController.cs
@@ -320,12 +320,7 @@ public class ARController : MonoBehaviour
     void Awake()
     {
         //Log(LogTag + "ARController.Awake())");
-        #if UNITY_IOS && !UNITY_EDITOR
-        ARNativePluginStatic.aruRequestCamera();
-        Thread.Sleep(2000);
-        #endif
 
-        InitializeAR ();
     }
 
     void OnEnable()
@@ -378,8 +373,14 @@ public class ARController : MonoBehaviour
     
     void Start()
     {
+#if UNITY_IOS && !UNITY_EDITOR
+        ARNativePluginStatic.aruRequestCamera();
+        Thread.Sleep(2000);
+#endif
+
+        InitializeAR();
         //Log(LogTag + "ARController.Start()");
-        
+
         // Ensure ARMarker objects that were instantiated/deserialized before the native interface came up are all loaded.
         ARMarker[] markers = FindObjectsOfType(typeof(ARMarker)) as ARMarker[];
         foreach (ARMarker m in markers) {
