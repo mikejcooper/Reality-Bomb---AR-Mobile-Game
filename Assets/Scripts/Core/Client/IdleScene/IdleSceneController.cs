71ca85602ec46e3a38259d58f4e3b6d2d0ed861d
Mike Cooper <mikecooper@Mikes-MacBook-Pro.local>
2017-03-05 18:00:59 +0000

    Rotating car object

--- Assets/Scripts/Core/Client/IdleScene/IdleSceneController.cs
+++ Assets/Scripts/Core/Client/IdleScene/IdleSceneController.cs
@@ -3,41 +3,40 @@ using System.Collections;
 
 public class IdleSceneController : MonoBehaviour
 {
-	private Camera CameraObject;
+	public GameObject Car;
 
+	private Camera _cameraObject;
 	private Transform _lookAt;
-	private Vector3 _startPosition;
 
-	private float _speed = 0.2f;
+	private Vector3 _cameraStartPosition;
+
+	private float _speed = 0.3f;
 	private float _width = 3;
-	private float _height = 0.2f;
+	private float _height = 0.5f;
 	private float _z = 1;
+	private float _timeCounter = 0;
 
 
 
 	void Start ()
 	{
-		CameraObject = Camera.main;
-		_startPosition = CameraObject.transform.position;
+		// Camera
+		_cameraObject = Camera.main;
+		_cameraStartPosition = _cameraObject.transform.position;
 		_lookAt = FindObjectOfType<IdleSceneController> ().transform;
 	}
-//	
-//	void Update ()
-//	{
-//		Vector3 axis = new Vector3 (0, 1, 0);
-//		CameraObject.transform.RotateAround(ObjectsRotatePoint, axis, PanSpeed * Time.deltaTime);
-//	}
-//
-
-	float timeCounter = 0;
-
+		
 	void Update () {
-		timeCounter += Time.deltaTime * _speed;
-		float x = Mathf.Cos (timeCounter) * _width;
-		float y = Mathf.Sin (timeCounter) * _height;
-		float z = Mathf.Sin (timeCounter) * _z;
-		CameraObject.transform.position = new Vector3 (x, y, z) + _startPosition;
-		CameraObject.transform.LookAt(_lookAt);
+		// Camera Movement
+		_timeCounter += Time.deltaTime * _speed;
+		float x = Mathf.Sin (_timeCounter) * _width;
+		float y = Mathf.Cos (_timeCounter) * _height;
+		float z = Mathf.Sin (0.5f*_timeCounter) * _z;
+		_cameraObject.transform.position = new Vector3 (x, y, z) + _cameraStartPosition;
+		_cameraObject.transform.LookAt(_lookAt);
+
+		// Car Movement
+		Car.transform.Rotate(Vector3.up * _timeCounter, 0.2f);
 	}
 }
 
