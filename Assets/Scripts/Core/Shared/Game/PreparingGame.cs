56237ba47837867f8c5b5fb6ba66bd607bc61e66
Mike Cooper <mikecooper@Mikes-MacBook-Pro.local>
2017-03-05 20:44:58 +0000

    321 go text changed

--- Assets/Scripts/Core/Shared/Game/PreparingGame.cs
+++ Assets/Scripts/Core/Shared/Game/PreparingGame.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using ServerLifecycle;
+using TMPro;
+
 
 
 public class PreparingGame : MonoBehaviour
@@ -16,31 +18,32 @@ public class PreparingGame : MonoBehaviour
 
 	void Start () {
 		_entry = GameObject.Instantiate (LeaderboardEntryPrefab);
-		_entry.transform.Find ("Waiting").GetComponent<Text> ().text = "Waiting for players...";
+		_entry.transform.Find ("Waiting").GetComponent<TextMeshProUGUI> ().text = "Waiting for players...";
 		_entry.transform.parent = transform;
 		StartCoroutine(StartCountDown ());
 	}
 
 	IEnumerator StartCountDown() {
-		float t1 = 1.0f; // time hide and show
+		float t1 = 1.2f; // time hide and show
 		yield return new WaitForSeconds (t1);
-		_entry.transform.Find ("Waiting").GetComponent<Text> ().text = "";
-		_entry.transform.Find ("Numbers").GetComponent<TextMesh> ().text = "3";
+		_entry.transform.Find ("Waiting").GetComponent<TextMeshProUGUI> ().text = "";
 		yield return new WaitForSeconds (t1);
-		_entry.transform.Find ("Numbers").GetComponent<TextMesh> ().text = "2";
+		_entry.transform.Find ("Numbers").GetComponent<TextMeshProUGUI> ().text = "3";
 		yield return new WaitForSeconds (t1);
-		_entry.transform.Find ("Numbers").GetComponent<Text> ().text = "1";
+		_entry.transform.Find ("Numbers").GetComponent<TextMeshProUGUI> ().text = "2";
 		yield return new WaitForSeconds (t1);
-		_entry.transform.Find ("Numbers").GetComponent<Text> ().text = "";
-		_entry.transform.Find ("Go").GetComponent<Text> ().text = "Go!";
+		_entry.transform.Find ("Numbers").GetComponent<TextMeshProUGUI> ().text = "1";
 		yield return new WaitForSeconds (t1);
-		_entry.transform.Find ("Go").GetComponent<Text> ().text = "";
+		_entry.transform.Find ("Numbers").GetComponent<TextMeshProUGUI> ().text = "";
+		_entry.transform.Find ("Go").GetComponent<TextMeshProUGUI> ().text = "Go!";
+		yield return new WaitForSeconds (t1);
+		_entry.transform.Find ("Go").GetComponent<TextMeshProUGUI> ().text = "";
 		if(CountDownFinishedEvent != null)
 			CountDownFinishedEvent ();
 	}
 
 	public void StartGameCountDown(){
-		_entry.transform.Find ("Waiting").GetComponent<Text> ().text = "";
+		_entry.transform.Find ("Waiting").GetComponent<TextMeshProUGUI> ().text = "";
 		StartCoroutine(StartCountDown ());
 	}
 
