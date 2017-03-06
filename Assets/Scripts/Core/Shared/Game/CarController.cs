6a2e42a9288ef17b634ce763077a142bf5e9a797
Matthew Duffin <matthewjduffin@gmail.com>
2017-03-06 15:03:26 +0000

    Proper synchronisation of bomb timer.

--- Assets/Scripts/Core/Shared/Game/CarController.cs
+++ Assets/Scripts/Core/Shared/Game/CarController.cs
@@ -38,7 +38,7 @@ public class CarController : NetworkBehaviour
 	private Text LifetimeText;
 	private bool _initialised;
 	private bool _controlsDisabled;
-	private bool _preparingGame;
+	private bool _preparingGame = true;
 
 	private void Start ()
 	{
@@ -91,7 +91,7 @@ public class CarController : NetworkBehaviour
 				CmdRequestColour ();
 //				EnableControls (false);
 			} else {
-				_preparingGame = true;
+				//_preparingGame = true;
 				// register
 				DebugConsole.Log ("GameManager.Instance.AddCar (gameObject);");
 				GameObject.FindObjectOfType<GameManager> ().AddCar (gameObject);
@@ -138,7 +138,7 @@ public class CarController : NetworkBehaviour
 
 	private void processSetBombMessage(bool isBomb) {
 		DebugConsole.Log("isBomb: " + isBomb);
-		HasBomb = isBomb;
+        HasBomb = isBomb;
 		if (isBomb) {
 			GameObject.FindObjectOfType<GameManager> ().BombObject.transform.parent = transform;
 			GameObject.FindObjectOfType<GameManager> ().BombObject.transform.localScale = 0.01f * Vector3.one;
@@ -162,7 +162,9 @@ public class CarController : NetworkBehaviour
 			return;
 				
 		if ((isLocalPlayer || IsPlayingSolo)) {
-            _healthBar.Value = Lifetime;
+            //_healthBar.Value = Lifetime;
+            if (!_preparingGame)
+                _healthBar.UpdateCountdown(Lifetime, HasBomb);
 
             if (CarProperties.PowerUpActive && Time.time > CarProperties.PowerUpEndTime) {
 				CarProperties.PowerUpActive = false;
@@ -278,14 +280,16 @@ public class CarController : NetworkBehaviour
 
 	[Server]
 	public void ServerGameStarting(){
-		DebugConsole.Log("Server: " + _preparingGame);
-		_preparingGame = false;
+        _preparingGame = false;
+        DebugConsole.Log("Server: " + _preparingGame);
 	}
 
 	[ClientRpc]
 	public void RpcPlayerGameStarting(){
+        _preparingGame = false;
 		DebugConsole.Log("Player: " + _preparingGame);
-		EnableControls (true);
+        _healthBar.UpdateCountdown(Lifetime, HasBomb);
+        EnableControls (true);
 	}
 
 	public void EnsureCarIsOnMap(){
