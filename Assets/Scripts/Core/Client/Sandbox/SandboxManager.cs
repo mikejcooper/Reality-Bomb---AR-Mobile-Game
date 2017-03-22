6744ead64aa55a7f533a7e6ba493a06717178d55
Rory Smith <roarsmith3@gmail.com>
2017-03-22 18:35:19 +0000

    Added sandbox tutorial dialogs.

--- Assets/Scripts/Core/Client/Sandbox/SandboxManager.cs
+++ Assets/Scripts/Core/Client/Sandbox/SandboxManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 using Powerups;
-
+using System.Collections.Generic;
 
 
 public class SandboxManager : MonoBehaviour
@@ -13,11 +13,13 @@ public class SandboxManager : MonoBehaviour
 	public GameObject CarObject;
 	public GameObject PlaneObject;
 	public GameObject TxtObjectPrefab;
-	public CanvasMessages CanvasMessage;
+	public Canvas CanvasObj;
+	public List<GameObject> TutorialDialogPrefabs;
 
+	private int _currentTutorialStage = 0;
+	private GameObject _currentDialog;
 
-	private bool _controlsDisabled;
-	private string _welcome_Txt = "\tWelcome to the Tutorial!\nUse the joystick to drive around but don't fall off the map!\nSee if you can pickup some Power Ups along the way!";
+//	private string _welcome_Txt = "\tWelcome to the Tutorial!\nUse the joystick to drive around but don't fall off the map!\nSee if you can pickup some Power Ups along the way!";
 	private string _splatter_Txt = "You've Activated the Splatter Power Up!\nThis splatters ink on your opponents' screens\nas shown below making it harder for them to see!";
 	private string _speed_Txt = "You've Activated the Speed Boost Power Up!\nEnjoy double the speed but be careful not to lose control!";
 	private string _respawn_Txt = "Oops!! You fell off the map! Don't Worry, You will\nbe respawned but you wont be able to move for 5 secs.\nBe careful or become an easy target!";
@@ -26,8 +28,13 @@ public class SandboxManager : MonoBehaviour
 	private Rect TxtRect;
 
 	void Start(){
-		TxtObjectPrefab.transform.Find ("place").GetComponent<TextMeshProUGUI> ().text = _welcome_Txt;
-		_controlsDisabled = false;
+		if (TutorialDialogPrefabs.Count > 0) {
+			SetCurrentDialog (0);
+		}
+
+		Debug.LogError ("TutorialDialogPrefabs.count: " + TutorialDialogPrefabs.Count);
+
+//		TxtObjectPrefab.transform.Find ("place").GetComponent<TextMeshProUGUI> ().text = _welcome_Txt;
 		SandBoxPowerUpManager.SpeedBoostActivatedEvent += SetSpeedTxt;
 		SandBoxPowerUpManager.InkSplatterActivatedEvent += SetSplatTxt;
 	}
@@ -35,6 +42,26 @@ public class SandboxManager : MonoBehaviour
 	void OnDestroy(){
 		SandBoxPowerUpManager.SpeedBoostActivatedEvent -= SetSpeedTxt;
 		SandBoxPowerUpManager.InkSplatterActivatedEvent -= SetSplatTxt;
+	}
+
+	private void SetCurrentDialog (int index) {
+		_currentTutorialStage = index;
+		_currentDialog = GameObject.Instantiate (TutorialDialogPrefabs [index]);
+		_currentDialog.transform.SetParent (CanvasObj.transform, false);
+		_currentDialog.gameObject.GetComponentInChildren<Button> ().onClick.AddListener (() => {
+			if (_currentTutorialStage + 1 < TutorialDialogPrefabs.Count) {
+				var oldDialog = _currentDialog;
+				SetCurrentDialog(_currentTutorialStage + 1);
+				Destroy(oldDialog);
+			} else {
+				Destroy(_currentDialog);
+				OnModalTutorialEnd();
+			}
+		});
+	}
+
+	private void OnModalTutorialEnd () {
+		
 	}
 
 	void Update(){
@@ -48,19 +75,9 @@ public class SandboxManager : MonoBehaviour
 	public void EnsureCarIsOnMap(){
 		if(CarObject.transform.position.y <= - 10.0f){
 			Reposition (PlaneObject);
-			DisableControls (5);
 		}
 	}
-
-	public void DisableControls(int seconds){
-		ToggleControls();
-		Invoke("ToggleControls", seconds);
-	}
-
-	public void ToggleControls(){
-		_controlsDisabled = !_controlsDisabled;
-	}
-
+		
 	public void Reposition(GameObject worldMesh)
 	{
 		Debug.Log ("Repositioning car");
