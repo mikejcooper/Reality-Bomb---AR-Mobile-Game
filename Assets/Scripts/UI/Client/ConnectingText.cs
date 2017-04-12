794e0e149b8931ea9291fb176baeb1971094fa0e
gchurch <gc14768@my.bristol.ac.uk>
2017-04-12 17:27:14 +0100

    Added UI for creating a nickname before joining the game

---
+++ Assets/Scripts/UI/Client/ConnectingText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectingText : MonoBehaviour {
+
+
+	private Text _textComponent;
+	private int _numberOfDots = 0;
+	private float _blinkRate = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		_textComponent = GetComponent<Text> ();
+	}
+
+	public void StartBlinking() {
+		InvokeRepeating ("Blink", 0.0f, 1 / _blinkRate);
+	}
+	
+	public void Blink() {
+		string newText = "connecting";
+		for (int i = 0; i < _numberOfDots; i++) {
+			newText += ".";
+		}
+		_textComponent.text = newText;
+		_numberOfDots = (_numberOfDots + 1) % 3;
+	}
+}
