fa96da80093ceb67eae858255387cf7c46b66fe1
gchurch <gc14768@my.bristol.ac.uk>
2017-04-15 14:06:40 +0100

    Added mute button for if you ever find the music annoying - god forbid. Default set to muted.

---
+++ Assets/MuteButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteButton : MonoBehaviour {
+
+	public bool Muted;
+	private Text _buttonText;
+
+	void Start() {
+		DontDestroyOnLoad (transform.parent.gameObject);
+		_buttonText = GetComponentInChildren<Text> ();
+		if (Muted == true) {
+			Muted = false;
+			ToggleMuteAllSound();
+		}
+	}
+
+	// Toggle all sound on and off
+	public void ToggleMuteAllSound() {
+		if (Muted) {
+			AudioListener.volume = 1.0f;
+			_buttonText.text = "Mute";
+		} else {
+			AudioListener.volume = 0.0f;
+			_buttonText.text = "Unmute";
+		}
+		Muted = !Muted;
+	}
+}
