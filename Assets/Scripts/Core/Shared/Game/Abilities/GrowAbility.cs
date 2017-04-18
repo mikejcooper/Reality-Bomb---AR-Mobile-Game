cd0efa2d25a8d32e6e6343e7c26ac7ea09513709
Rory Smith <roarsmith3@gmail.com>
2017-04-18 20:34:45 +0100

    Tidied up powerups and animations.

--- Assets/Scripts/Core/Shared/Game/Abilities/GrowAbility.cs
+++ Assets/Scripts/Core/Shared/Game/Abilities/GrowAbility.cs
@@ -15,41 +15,27 @@ namespace Abilities {
 		public const string TAG = "growability";
 		private const float SPEED_FACTOR = 1.5f;
 
-		protected GameObject _growObject;
-
 		private Vector3 _originalSize;
 		private Vector3 _scale;
 		private bool _maxGrow;
 
-
-		override protected void OnApplyCanvasEffect (Canvas canvas, bool triggeredPowerup) {
-			if (triggeredPowerup) {
-				
-			}
-		}
-
-		override protected void OnRemoveCanvasEffect (Canvas canvas, bool triggeredPowerup) {
-			if (triggeredPowerup) {
-				Destroy (_growObject);
-			}
-		}
-
 		protected override void OnApplyCarEffect (CarProperties properties, bool triggeredPowerup) {
-			_maxGrow = false;
+			if (triggeredPowerup) {
+				_maxGrow = false;
 
-			if (gameObject.transform.localScale.x < 1.5f) {
-				gameObject.transform.localScale *= 3f;
+				if (gameObject.transform.localScale.x < 1.5f) {
+					gameObject.transform.localScale *= 3f;
 
-				properties.MaxSpeed *= SPEED_FACTOR;
-				properties.Acceleration *= SPEED_FACTOR;
-			} else {
-				_maxGrow = true;
+					properties.MaxSpeed *= SPEED_FACTOR;
+					properties.Acceleration *= SPEED_FACTOR;
+				} else {
+					_maxGrow = true;
+				}
 			}
-
 		}
 
 		protected override void OnRemoveCarEffect (CarProperties properties, bool triggeredPowerup) {
-			if (!_maxGrow) {
+			if (triggeredPowerup && !_maxGrow) {
 				gameObject.transform.localScale /= 3f;
 
 				properties.MaxSpeed /= SPEED_FACTOR;
--- Assets/Scripts/Core/Shared/Game/Abilities/ShrinkAbility.cs
+++ Assets/Scripts/Core/Shared/Game/Abilities/ShrinkAbility.cs
@@ -13,44 +13,30 @@ namespace Abilities {
 	public class ShrinkAbility : BaseAbility<ShrinkAbilityProperties> {
 
 		public const string TAG = "shrinkability";
-		private const float SPEED_FACTOR = 1.5f;
-
-		protected GameObject _shrinkObject;
+		private const float SPEED_FACTOR = 1.5f;	
 
 		private Vector3 _originalSize;
 		private Vector3 _scale;
 		private bool _maxShrink;
 
-
-		override protected void OnApplyCanvasEffect (Canvas canvas, bool triggeredPowerup) {
-			if (triggeredPowerup) {
-
-			}
-		}
-
-		override protected void OnRemoveCanvasEffect (Canvas canvas, bool triggeredPowerup) {
-			if (triggeredPowerup) {
-				Destroy (_shrinkObject);
-			}
-		}
-
 		protected override void OnApplyCarEffect (CarProperties properties, bool triggeredPowerup) {
-			_maxShrink = false;
+			if (triggeredPowerup) {
+				_maxShrink = false;
 
-			if (gameObject.transform.localScale.x > 0.25f) {
-				gameObject.transform.localScale /= 2f;
+				if (gameObject.transform.localScale.x > 0.25f) {
+					gameObject.transform.localScale /= 2f;
 
-				properties.MaxSpeed /= SPEED_FACTOR;
-				properties.Acceleration /= SPEED_FACTOR;
-			} else {
-				_maxShrink = true;
+					properties.MaxSpeed /= SPEED_FACTOR;
+					properties.Acceleration /= SPEED_FACTOR;
+				} else {
+					_maxShrink = true;
+				}
 			}
-
 
 		}
 
 		protected override void OnRemoveCarEffect (CarProperties properties, bool triggeredPowerup) {
-			if (!_maxShrink) {
+			if (triggeredPowerup && !_maxShrink) {
 				gameObject.transform.localScale *= 2f;
 
 				properties.MaxSpeed *= SPEED_FACTOR;
