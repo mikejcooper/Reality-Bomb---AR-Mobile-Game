3758b89dba40b41c866f4118163fb80e20255f9c
Rory Smith <roarsmith3@gmail.com>
2017-04-20 20:43:01 +0100

    Added red glow on screen edge while you have the bomb.

--- Assets/Scripts/Core/Shared/Game/PlayerIndicationRenderer.cs
+++ Assets/Scripts/Core/Shared/Game/PlayerIndicationRenderer.cs
@@ -1,32 +1,49 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class PlayerIndicationRenderer : MonoBehaviour
 {
 
 	public GameObject YouPointer;
 	public GameObject BombObject;
+	public GameObject EdgeGlow;
 
 	public bool isMeIndicaterOn = true;
-	public bool isBombIndicaterOn = true;
+	public bool isBombIndicaterOn;
 	public Material material1;
 	public Material material2;
 
 	private bool authority = false;
 	private GameObject _initialisedYouPointer;
 	private GameObject _initialisedBomb;
+	private GameObject _initialisedGlow;
+
+	private CarController _controller;
+	private Image _glowImage;
 
 	void Start(){
 		Debug.Log("Starting Player Indication");
 		if (this.GetComponentInParent<CarController> () != null) {
 			Debug.Log ("CarController found");
-			this.GetComponentInParent<CarController>().OnSetBombEvent += setBombIndicator;
+			_controller = this.GetComponentInParent<CarController> ();
+			_controller.OnSetBombEvent += setBombIndicator;
 		}
-		_initialisedBomb = CreatePointer (BombObject);
+
+		_initialisedBomb = InstantiateAndDeactivate (BombObject);
+		_initialisedBomb.transform.localScale = new Vector3 (80.0f,80.0f,80.0f);
+		_initialisedBomb.transform.localPosition = new Vector3 (0.0f,2.0f,0.0f);
+
 		authority = IsOwnCarProperties (GetComponent<CarProperties> ());
 		if (authority) {
-			_initialisedYouPointer = CreatePointer (YouPointer);
+			_initialisedYouPointer = InstantiateAndDeactivate (YouPointer);
+			_initialisedYouPointer.transform.localScale = new Vector3(14.0f,60.0f,20.0f);
+			_initialisedYouPointer.transform.localRotation = Quaternion.identity * Quaternion.Euler(-90,0,0);
+			_initialisedYouPointer.SetActive (true);
+
+			_initialisedGlow = InstantiateAndDeactivate (EdgeGlow);
+			_glowImage = _initialisedGlow.GetComponent<Image> ();
 		}
 	}
 
@@ -43,44 +60,51 @@ public class PlayerIndicationRenderer : MonoBehaviour
 	void setMeIndicatorOff ()
 	{
 		isMeIndicaterOn = false;
-		_initialisedYouPointer.SetActive (false);
+		if (_initialisedYouPointer != null) {
+			_initialisedYouPointer.SetActive (false);
+		}
 	}
 
-	void setBombIndicator (bool bomb)
+	void setBombIndicator (bool hasBomb)
 	{
-		isBombIndicaterOn = bomb;
-		_initialisedBomb.SetActive (bomb);
+		isBombIndicaterOn = hasBomb;
+		_initialisedBomb.SetActive (hasBomb);
+
+		if (_initialisedGlow != null) {
+			_initialisedGlow.SetActive (hasBomb);
+		}
+
+		if (_initialisedYouPointer != null) {
+			if (hasBomb) {
+				_initialisedYouPointer.transform.localPosition = new Vector3 (0.0f, 2.0f, 0.0f);
+			} else {
+				_initialisedYouPointer.transform.localPosition = new Vector3 (0.0f, 0.0f, 0.0f);
+			}
+		}
+
+
 	}
 
 
 		
 	void Update(){
-		if (authority && isMeIndicaterOn) {
-			if (isBombIndicaterOn) {
-				_initialisedYouPointer.transform.localPosition = new Vector3 (0.0f, 2.0f, 0.0f);
-			} else {
-				_initialisedYouPointer.transform.localPosition = new Vector3 (0.0f, 0.0f, 0.0f);
-			}
-			_initialisedYouPointer.transform.localScale = new Vector3(14.0f,60.0f,20.0f);
-			_initialisedYouPointer.transform.localRotation = Quaternion.identity * Quaternion.Euler(-90,0,0);
-			_initialisedYouPointer.transform.parent = transform;
-			_initialisedYouPointer.SetActive (true);
-		}
 		if (isBombIndicaterOn) {
-			CarController car = this.GetComponentInParent<CarController> ();
-
-			float bombDangerLevel = 2.0f * (15.0f - car.Lifetime) / 15.0f;
-
-			_initialisedBomb.transform.parent = transform;
-			_initialisedBomb.transform.localScale = new Vector3 (80.0f,80.0f,80.0f);
-			_initialisedBomb.transform.localPosition = new Vector3 (0.0f,2.0f,0.0f);
+			
+			float bombDangerLevel = 2.0f * (15.0f - _controller.Lifetime) / 15.0f;
 			float lerp = Mathf.PingPong(bombDangerLevel * Time.time, 1.0f) / 1.0f;
 			_initialisedBomb.transform.GetChild (1).GetComponent<Renderer> ().material.Lerp(material1,material2,lerp);
+
+			if (_glowImage != null) {
+				var color = _glowImage.color;
+				color.a = 0.3f + Mathf.PingPong (Time.time, 1.0f - 0.3f);
+				_glowImage.color = color;
+			}
 		}
 	}
 		
-	private GameObject CreatePointer (GameObject prefab) {
+	private GameObject InstantiateAndDeactivate (GameObject prefab) {
 		GameObject obj = GameObject.Instantiate(prefab);
+		obj.transform.SetParent (transform, false);
 		obj.SetActive (false);
 		return obj;
 	}
