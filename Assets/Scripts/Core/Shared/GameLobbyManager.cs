923726742e244869bbf8d298c6b162d65ef788f7
Rory Smith <roarsmith3@gmail.com>
2017-04-26 20:08:30 +0100

    Fixed merge issues.

--- Assets/Scripts/Core/Shared/GameLobbyManager.cs
+++ Assets/Scripts/Core/Shared/GameLobbyManager.cs
@@ -12,19 +12,18 @@ public class GameLobbyManager : NetworkCompat.NetworkLobbyManager {
 	public delegate void OnLobbyClientConnected ();
 	public delegate void OnLobbyClientDisconnected ();
 	public delegate void OnLobbyClientReadyToBegin ();
-	public delegate void OnLobbyClientGameLoaded ();
 	public delegate void OnLobbyClientGameLoaded (NetworkConnection conn);
-  public delegate void OnMeshClearToDownloadCallback(string address, int port);
+  	public delegate void OnMeshClearToDownloadCallback(string address, int port);
 	public delegate void OnStartGameCountdownCallback (int delay);
 	public delegate void OnCancelGameCountdownCallback (string reason);
 
-  public event OnLobbyServerConnected OnLobbyServerConnectedEvent;
+  	public event OnLobbyServerConnected OnLobbyServerConnectedEvent;
 	public event OnLobbyServerDisconnected OnLobbyServerDisconnectedEvent;
 	public event OnLobbyClientConnected OnLobbyClientConnectedEvent;
 	public event OnLobbyClientDisconnected OnLobbyClientDisconnectedEvent;
 	public event OnLobbyClientReadyToBegin OnLobbyClientReadyToBeginEvent;
 	public event OnLobbyClientGameLoaded OnLobbyClientGameLoadedEvent;
-  public event OnMeshClearToDownloadCallback OnMeshClearToDownloadEvent;
+  	public event OnMeshClearToDownloadCallback OnMeshClearToDownloadEvent;
 	public event OnStartGameCountdownCallback OnStartGameCountdownEvent;
 	public event OnCancelGameCountdownCallback OnCancelGameCountdownEvent;
 
