b59d631a09fd879803c33e485cc3793d29cfa20d
Matthew Duffin <matthewjduffin@gmail.com>
2017-04-27 12:50:36 +0100

    Fixed null reference exception

--- Assets/Scripts/Core/Server/Audio/MuteButton.cs
+++ Assets/Scripts/Core/Server/Audio/MuteButton.cs
@@ -7,22 +7,22 @@ public class MuteButton : MonoBehaviour {
 	
 	private Text _buttonText;
 
+    /*
 	void Start() {
 		_buttonText = GetComponentInChildren<Text>();
 		if(AudioListener.volume == 0.0f) {
 			_buttonText.text = "Unmute (m)";
 		}
 	}
+    */
 
 	//Toggle all sound on and off
 	public void ToggleMuteSound() {
 		if(AudioListener.volume == 0.0f) {
 			AudioListener.volume = 1.0f;
-			_buttonText.text = "Mute (m)";
 		}
 		else {
 			AudioListener.volume = 0.0f;
-			_buttonText.text = "Unmute (m)";
 		}
 	}
 }
