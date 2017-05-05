4ef2ab4f8bd2ec5d3ba71b5dee8c0ae1718f65f1
Matthew Duffin <matthewjduffin@gmail.com>
2017-05-05 23:41:32 +0100

    Power-ups now spin out when they are done.

---
+++ Assets/EndSpinOut.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndSpinOut : MonoBehaviour {
+    public delegate void OnEndSpinOut();
+    public event OnEndSpinOut OnEndSpinOutEvent;
+    
+    private void OnEnable()
+    {
+        transform.localScale = Vector3.one;
+        //transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+    }
+
+    public void OnEndSpinOutCaller()
+    {
+        Debug.Log("Calling event");
+        OnEndSpinOutEvent();
+    }
+
+}
--- Assets/Scripts/Core/Shared/Game/Powerups/PowerUpUnspawner.cs
+++ Assets/Scripts/Core/Shared/Game/Powerups/PowerUpUnspawner.cs
@@ -7,23 +7,56 @@ namespace Powerups
     {
         //private SpawnPool _powerUpPool;
         private float _startTime;
-        private float maxTime = 20;
+        private float maxTime = 10;
+        private bool timeUp = false;
+
+        private ParticleSystem _ps;
+
+        private void Start()
+        {
+            GetComponentInChildren<EndSpinOut>().OnEndSpinOutEvent += Recycle;
+            _ps = GetComponentInChildren<ParticleSystem>();
+        }
 
         private void OnEnable()
         {
             _startTime = Time.time;
+            timeUp = false;
+            _ps.Play();
+        }
+
+        private void OnDestroy()
+        {
+            //Not sure this really matters when we are destroying the same gameobject
+            GetComponentInChildren<EndSpinOut>().OnEndSpinOutEvent -= Recycle;
         }
 
         public void Update()
         {
             float elapsed = Time.time - _startTime;
             
-            if (elapsed > maxTime && (isServer || !NetworkServer.active))
+            if (elapsed > maxTime-1 && _ps.isEmitting)
+                _ps.Stop();
+            if (elapsed > maxTime && !timeUp)
             {
-                FindObjectOfType<BasePowerUpManager>().PowerUpPool.UnSpawnObject(gameObject);
-                if (NetworkServer.active)
-                    NetworkServer.UnSpawn(gameObject);
+                Animation anim = GetComponentInChildren<Animation>();
+                
+                anim.Play("SpinOut");
+                timeUp = true;                
             }                      
         }
+        
+        public void Recycle()
+        {
+            
+            if (!isServer && NetworkServer.active)
+                return;
+
+            FindObjectOfType<BasePowerUpManager>().PowerUpPool.UnSpawnObject(gameObject);
+            if (NetworkServer.active)
+                NetworkServer.UnSpawn(gameObject);
+                
+        }
+    
     }
 }
