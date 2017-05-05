056696ffb86be69e776257754d8b48e802ddd1d7
Rory Smith <roarsmith3@gmail.com>
2017-05-05 19:52:51 +0100

    Removed null reference to deleted buttons.

--- Assets/Scripts/UI/Server/ServerIdleGUI.cs
+++ Assets/Scripts/UI/Server/ServerIdleGUI.cs
@@ -4,27 +4,7 @@ using UnityEngine;
 using ServerLifecycle;
 
 public class ServerIdleGUI : MonoBehaviour {
-
-	public UnityEngine.UI.Selectable startGameButton;
-	public UnityEngine.UI.Selectable instantStartGameButton;
-	public UnityEngine.UI.Selectable loadMeshButton;
-
-	void Start () {
-		startGameButton.GetComponent<UnityEngine.UI.Button> ().onClick.AddListener(() => { 
-			switch (ServerSceneManager.Instance.CurrentState()) {
-			case ProcessState.PreparingGame:
-				ServerSceneManager.Instance.OnServerRequestGameStart (5);
-				break;
-			case ProcessState.CountingDown:
-				ServerSceneManager.Instance.OnServerRequestCancelGameStart ();
-				break;
-			}
-
-		});
-		instantStartGameButton.GetComponent<UnityEngine.UI.Button> ().onClick.AddListener(OnInstantStart);
-		loadMeshButton.GetComponent<UnityEngine.UI.Button> ().onClick.AddListener(OnLoadMesh);
-	}
-
+	
 	private void OnInstantStart () {
 		ServerSceneManager.Instance.OnServerRequestGameStart (0);
 	}
